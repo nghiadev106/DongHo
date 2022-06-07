1112853f649c99e6d1d77956d1b2fcd8058069a2
nghiadv <nghiado106.dev@gmail.com>
2022-06-07 08:54:11 +0700

    update

--- NuocHoa/Areas/Admin/Models/Order/StatusRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NuocHoa.Areas.Admin.Models.Order
-{
-    public class StatusRequest
-    {
-        public long Id { get; set; }
-        public int? PaymentStatus { get; set; }
-    }
-}
